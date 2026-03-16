5911f4c6e2f1726e237cdda55d382a18b0233b2e
yangpan2022 <9948708118@qq.com>
2026-03-16 12:19:33 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/example/studyroom/StudyRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRoomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRoomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyroom/StudyRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
